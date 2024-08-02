a3a390cbac89f674a6264d01dbf81d101ca3e181
DwashKarma <dwaskc@gmail.com>
2024-08-02 19:45:52 +0545

    folder structure and navbar

--- app/(protected-routes)/page.tsx
+++ app/(protected-routes)/page.tsx
@@ -1,5 +1,10 @@
+import NavBar from "@/components/navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
