95e5364f3f331ab7d562eb97c6948128b53a1048
dt3280097@gmail.com <dt3280097@gmail.com>
2024-08-03 20:03:32 +0545

    navbar and card code

--- app/(protected-routes)/page.tsx
+++ app/(protected-routes)/page.tsx
@@ -1,3 +1,4 @@
+import CourseCard from "@/components/courseCard";
 import NavBar from "@/components/navbar";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <NavBar />
+      <CourseCard/>
     </div>
   );
 }
