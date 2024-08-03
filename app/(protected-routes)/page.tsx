199bf35bf0f10b5dee6d48f94be09dfbf1b9d637
DwashKarma <dwaskc@gmail.com>
2024-08-03 11:49:03 +0545

    mongo db connection and next auth added

--- app/(protected-routes)/page.tsx
+++ app/(protected-routes)/page.tsx
@@ -1,10 +1,26 @@
+"use client";
 import NavBar from "@/components/navbar";
-import Image from "next/image";
-
+import { connectDB } from "@/libs/mongodb";
+import { useSession, signIn, signOut } from "next-auth/react";
 export default function Home() {
+  const db = connectDB();
+  const { data: session, status } = useSession();
+  console.log(session);
   return (
-    <div>
-      <NavBar />
+    <div className="flex flex-col">
+      {/* <NavBar /> */}
+      <h1>Home</h1>
+      <b>signIn</b>
+      <div>
+        <button className="bg-sky-700" onClick={() => signIn("google")}>
+          SignIn
+        </button>
+      </div>
+      <div>
+        <button className="bg-sky-700" onClick={() => signOut()}>
+          Signout
+        </button>
+      </div>
     </div>
   );
 }
---
+++ modals/User.ts
@@ -0,0 +1,44 @@
+import { model, models, Schema } from "mongoose";
+
+export interface UserDocument {
+  email: string;
+  password: string;
+  name: string;
+  image: string;
+  id: string;
+}
+
+const UserSchema = new Schema<UserDocument>(
+  {
+    id: {
+      type: String,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email is required"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Email is invalid",
+      ],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    image: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// if there is no model user then create user model with this schema  else use model user
+const User = models.Users || model<UserDocument>("Users", UserSchema);
+export default User;
