fb8eedaceeabc84ec910fadbe6b3ee8857918889
DwashKarma <dwaskc@gmail.com>
2024-08-04 20:46:36 +0545

    refactor: navabar  design

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,101 +1,105 @@
-'use client';
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-// import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import '../images/Dlogo.png';
+"use client";
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import "../images/Dlogo.png";
 import Image from "next/image";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { TextField } from "@mui/material";
 
 const options = [
-    'None',
-    'Atria',
-    'Callisto',
-    'Dione',
-    'Ganymede',
-    'Hangouts Call',
-    'Luna',
-    'Oberon',
-    'Phobos',
-    'Pyxis',
-    'Sedna',
-    'Titania',
-    'Triton',
-    'Umbriel',
-  ];
-  
-  const ITEM_HEIGHT = 48;
-  
-  const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
+  "None",
+  "Atria",
+  "Callisto",
+  "Dione",
+  "Ganymede",
+  "Hangouts Call",
+  "Luna",
+  "Oberon",
+  "Phobos",
+  "Pyxis",
+  "Sedna",
+  "Titania",
+  "Triton",
+  "Umbriel",
+];
+
+const ITEM_HEIGHT = 48;
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "55ch",
     },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  }));
-  
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-  
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: '55ch',
-      },
-    },
-  }));
-  
-  const MenuButton = styled(IconButton)(({ theme }) => ({
-    display: 'none',
-    [theme.breakpoints.down('md')]: {
-      display: 'flex',
-    },
-  }));
-{/*for the signout  */}
-  interface NavBarProps{
-    session:any;
-    signOut: () => void;
+  },
+}));
+
+const MenuButton = styled(IconButton)(({ theme }) => ({
+  display: "none",
+  [theme.breakpoints.down("md")]: {
+    display: "flex",
+  },
+}));
+{
+  /*for the signout  */
+}
+interface NavBarProps {
+  session: any;
+  signOut: () => void;
+}
+const Navbar: React.FC<NavBarProps> = ({ signOut, session }) => {
+  const [image, setImage] = useState<string>("");
+  useEffect(() => {
+    setImage(session?.user?.image);
+  }, [session]);
+  {
+    /*Ending  logic of signout  */
   }
-const Navbar: React.FC<NavBarProps> = ({signOut,session}) => {
-    const [image, setImage] = useState<string>("");
-    useEffect(() => {
-        setImage(session?.user?.image);
-      }, [session]);
-      {/*Ending  logic of signout  */}
 
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -117,128 +121,109 @@ const Navbar: React.FC<NavBarProps> = ({signOut,session}) => {
     setAnchorEl(null);
   };
 
-
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <MenuButton
-            color="inherit"
-            aria-label="menu"
-            onClick={handleMenuClick}
-          >
-            <MenuIcon />
-          </MenuButton>
-        <Box
-  sx={{
-    flexGrow: 1,
-    display: { xs: 'none', sm: 'block' },
-    width: 100,  // Adjust width as needed
-    height: 'auto',  // Maintain aspect ratio
-  }}
->
-        <Image
-          className="rounded-full"
-          src={image}
-          alt="ProfileImage"
-          height={80}
-          width={40} 
-        />
-        <h2>{session?.user?.email}</h2>
-         </Box>
-
-          <Button
-            aria-label="categories"
-            id="categories-button"
-            aria-controls={menuOpen ? 'categories-menu' : undefined}
-            aria-expanded={menuOpen ? 'true' : undefined}
-            aria-haspopup="true"
-            onClick={handleMenuButtonClick}
-            color="inherit"
-            sx={{ display: { xs: 'none', md: 'flex' } }}
-          >
-            Categories
-          </Button>
-          <Menu
-            id="categories-menu"
-            MenuListProps={{
-              'aria-labelledby': 'categories-button',
-            }}
-            anchorEl={anchorEl}
-            open={menuOpen}
-            onClose={handleMenuClose}
-            PaperProps={{
-              style: {
-                maxHeight: ITEM_HEIGHT * 4.5,
-                width: '20ch',
-              },
-            }}
-          >
-            {options.map((option) => (
-              <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleMenuClose}>
-                {option}
-              </MenuItem>
-            ))}
-          </Menu>
-          <Box sx={{ flexGrow: 1, display: { md: 'none', md: 'flex' } }}>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ 'aria-label': 'search' }}
-              />
-            </Search>
-          </Box>
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+    <div>
+      <div className="flex justify-around items-center p-4  text-slate-600">
+        <MenuButton color="inherit" aria-label="menu" onClick={handleMenuClick}>
+          <MenuIcon />
+        </MenuButton>
+        <div className="flex gap-10 items-center">
+          <div className="flex items-center gap-4">
+            <Image
+              className="rounded-full"
+              src={image}
+              alt="ProfileImage"
+              height={80}
+              width={40}
+            />
+            <h2>{session?.user?.email}</h2>
+          </div>
+          <div>
             <Button
-              onClick={signOut}
+              aria-label="categories"
+              id="categories-button"
+              aria-controls={menuOpen ? "categories-menu" : undefined}
+              aria-expanded={menuOpen ? "true" : undefined}
+              aria-haspopup="true"
+              onClick={handleMenuButtonClick}
               color="inherit"
+              sx={{ display: { xs: "none", md: "flex" } }}
             >
-              Sign Out
+              Categories
             </Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        anchor='left'
-        open={drawerOpen}
-        onClose={handleDrawerClose}
-      >
+            <Menu
+              id="categories-menu"
+              MenuListProps={{
+                "aria-labelledby": "categories-button",
+              }}
+              anchorEl={anchorEl}
+              open={menuOpen}
+              onClose={handleMenuClose}
+            >
+              {options.map((option) => (
+                <MenuItem
+                  key={option}
+                  selected={option === "Pyxis"}
+                  onClick={handleMenuClose}
+                >
+                  {option}
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <TextField
+            type="text"
+            name="search"
+            label="search"
+            placeholder="Search"
+          />
+        </div>
+        <div>
+          <button
+            className="bg-green-700 text-white p-2 hover:bg-green-800 rounded-md hover:shadow-lg"
+            onClick={signOut}
+          >
+            Sign Out
+          </button>
+        </div>
+      </div>
+      <Drawer anchor="left" open={drawerOpen} onClose={handleDrawerClose}>
         <Box
           sx={{ width: 250 }}
           role="presentation"
           onClick={handleDrawerClose}
           onKeyDown={handleDrawerClose}
         >
-
           {/* code for the responsive or repsonsive for the small sizes*/}
           <List>
-          <Box
-  sx={{
-    flexGrow: 1,
-    display: { xs: 'block', sm: 'block' },
-    width: 100,  // Adjust width as needed
-    height: 'auto',  // Maintain aspect ratio
-  }}
->
-        <Image
-          className="rounded-full"
-          src={image}
-          alt="ProfileImage"
-          height={80}
-          width={40} 
-        />
-        <h2>{session?.user?.email}</h2>
-         </Box>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "block", sm: "block" },
+                width: 100, // Adjust width as needed
+                height: "auto", // Maintain aspect ratio
+              }}
+            >
+              <Image
+                className="rounded-full"
+                src={image}
+                alt="ProfileImage"
+                height={80}
+                width={40}
+              />
+              <h2>{session?.user?.email}</h2>
+            </Box>
             <ListItem button onClick={signOut}>
               <ListItemText primary="Sign Out" />
             </ListItem>
             <Divider />
             <ListItem button>
-              <ListItemText primary="Categories" onClick={handleMenuButtonClick}
- />
+              <ListItemText
+                primary="Categories"
+                onClick={handleMenuButtonClick}
+              />
             </ListItem>
             <ListItem>
               <Search>
@@ -247,16 +232,15 @@ const Navbar: React.FC<NavBarProps> = ({signOut,session}) => {
                 </SearchIconWrapper>
                 <StyledInputBase
                   placeholder="Search…"
-                  inputProps={{ 'aria-label': 'search' }}
+                  inputProps={{ "aria-label": "search" }}
                 />
               </Search>
             </ListItem>
           </List>
         </Box>
       </Drawer>
-    </Box>
+    </div>
   );
-  
-}
+};
 
-export default Navbar
+export default Navbar;
