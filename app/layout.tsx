2306fed71268c419d8e9c215982ab272ec3f0d58
DwashKarma <dwaskc@gmail.com>
2024-08-09 13:56:12 +0545

    refactor : login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import Provider from "@/providers/SessionProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "LMS",
--- components/login/index.tsx
+++ components/login/index.tsx
@@ -9,11 +9,13 @@ import * as yup from "yup";
 const loginValues = {
   email: "",
   password: "",
+  fullname: "",
 };
 
 const loginSchema = yup.object().shape({
-  email: yup.string().required("Please fill out the field!"),
-  password: yup.string().required("Please fill out the field!"),
+  email: yup.string().required("Please enter your email !"),
+  password: yup.string().required("Please enter your password !"),
+  fullname: yup.string().required("Please enter your name!"),
 });
 const LoginPage = () => {
   const { values, handleBlur, handleChange, touched, errors, handleSubmit } =
@@ -32,6 +34,19 @@ const LoginPage = () => {
 
   return (
     <div className="shadow-md  shadow-slate-200 rounded p-8 w-fit grid gap-8">
+      <h2 className="text-center font-normal text-xl uppercase text-primary">
+        Login Page
+      </h2>
+      <InputField
+        name="fullname"
+        value={values.fullname}
+        label="Full Name"
+        type="text"
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        helperText={touched.fullname ? errors.fullname : ""}
+        errors={touched.fullname && Boolean(errors.fullname)}
+      />
       <InputField
         name="email"
         value={values.email}
@@ -53,7 +68,14 @@ const LoginPage = () => {
         helperText={touched.password ? errors.password : ""}
         errors={touched.password && Boolean(errors.password)}
       />
-      <ButtonComponent handleClick={handleSubmit}>Login</ButtonComponent>
+      <div className="text-end grid gap-2">
+        <ButtonComponent handleClick={handleSubmit}>Login</ButtonComponent>
+        <div>
+          <span className="text-end text-sky-800 hover:border-b cursor-pointer hover:border-b-sky-800 text-xs font-normal">
+            Already have an account
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
